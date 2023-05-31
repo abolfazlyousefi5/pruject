933ff640d83eb4f0729b97645069ac601beaf25c
abolfazl <afgboy2922@gmail.com>
2023-05-31 12:09:40 +0330

    aded .js

--- Project-js/test.js
+++ Project-js/test.js
@@ -1,6 +1,32 @@
-var test="abolfazl"+18;
 
-var test2=45+22+"aboli"
+var today=new Date().getDay();
+var  day;
 
-var persent={firstname:"abol" , lastname:"yousefi"}
-document.write(persent.lastname);
+console.log(today);
+switch (today) {
+    case 0:
+        day="Saturday";
+             break;
+    case 1:
+        day="Sunday";
+             break;
+    case 2:
+        day="Monday";
+             break;
+    case 3:
+        day="Tuesday";
+             break;
+    case 4:
+        day="Wednesday";
+             break;
+    case 5:
+        day="Thursday";
+             break;
+    case 6:
+        day="Friday";
+           
+            }
+             Document.while ("today is :" + day);
+
+                
+             
